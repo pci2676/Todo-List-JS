bf7f4730b03c8ecfddbced61455797aec2761888
chanin <pci2676@gmail.com>
2020-03-06 03:04:58 +0900

    boilerplate 경로 재정의

--- step-js/app/app.js
+++ step-js/app/app.js
@@ -1,4 +1,11 @@
+import TodoStorage from "./js/TodoStorage.js";
+import TodoService from "./js/TodoService.js";
+import TodoController from "./js/TodoController.js";
+
 function App() {
+    this.todoStorage = new TodoStorage();
+    this.todoService = new TodoService(this.todoStorage);
+    this.todoController = new TodoController(this.todoService);
 }
 
 new App();
